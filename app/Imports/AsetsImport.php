62c112a26efd1aed4dd86b65fae705d998b62779
laveniav <vegalaveniaaa@gmail.com>
2024-12-14 22:38:59 +0700

    update import

--- app/Imports/AsetsImport.php
+++ app/Imports/AsetsImport.php
@@ -8,6 +8,10 @@ use App\Models\asal;
 use App\Models\Asset;
 use App\Models\Jenis;
 use App\Models\Kategori;
+use App\Models\Klasifikasi;
+use App\Models\objek;
+use App\Models\status;
+use App\Models\Unit;
 use App\Models\User;
 
 use Maatwebsite\Excel\Concerns\ToModel;
@@ -32,22 +36,47 @@ class AsetsImport implements ToModel, WithHeadingRow
     public function model(array $row)
     {
 
-        $existingAsset = Asset::where('kode_barang', $row['kode_barang'])->first();
+        $existingAsset = Asset::where('kode_barang', $row['kode_barang'])
+        ->where('no_register', $row['no_register'])
+        ->where(function ($query) use ($row) {
+            if ($row['merk'] !== null) {
+                $query->where('merk', $row['merk']);
+            }
+        })
+            ->where('thn_pmbelian', $row['thn_pmbelian'])
+            ->first();
 
         if ($existingAsset) {
-            $this->existingData[] = $row['kode_barang'];
+            $this->existingData[] = $row['no_register'];
             return null;
         }
 
-        return new Asset([
-            'nama_barang'  => $row['nama_barang'],
-            'kode_barang'  => $row['kode_barang'],
-            'jenis_id'     => Jenis::where('nama_jenis', $row['jenis_id'])->value('id'),
-            'kategori_id'  => Kategori::where('nama_kategori', $row['kategori_id'])->value('id'),
-            'asal_id'      => Asal::where('nama_asal', $row['asal_id'])->value('id'),
-            'status_asset' => "Aset terkini",
-        ]);
-        
+        if (is_numeric($row['no'])) {
+            return new Asset([
+                'nama_barang'  => $row['nama_barang'],
+                'kode_barang'  => $row['kode_barang'],
+                'no_register'  => $row['no_register'],
+                'merk'         => $row['merk'],
+                'bahan'        => $row['bahan'],
+                'thn_pmbelian' => $row['thn_pmbelian'],
+                'pabrik'       => $row['pabrik'],
+                'rangka'       => $row['rangka'],
+                'mesin'        => $row['mesin'],
+                'polisi'       => $row['polisi'],
+                'bpkb'         => $row['bpkb'],
+                'asal_id'      => Asal::where('asal_asset', $row['asal_usul'])->value('id'),
+                'harga'        => $row['harga'],
+                'deskripsi_brg' => $row['deskripsi_brg'],
+                'keterangan'   => $row['keterangan'],
+                'opd'          => $row['opd'],
+                'status_id'    => Status::where('status_asset', 'Asset Terkini')->value('id'),
+                'objek_id'     => null,
+                'unit_id'      => null,
+                'klasifikasi_id' => Klasifikasi::where('nama_klasifikasi', 'Extra Countable')->value('id'),
+            ]);
+        }
+
+        return null;
     }
 
     public function getExistingData()
